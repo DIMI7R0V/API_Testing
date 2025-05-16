9efe942b515e90e23404af0756152b9ec5517acd
DIMI7R0V <danito1718@gmail.com>
2025-05-16 19:59:08 +0300

    f

--- Final_APITesting/Final_APITesting/Tests/UsersTest.cs
+++ Final_APITesting/Final_APITesting/Tests/UsersTest.cs
@@ -1,6 +1,7 @@
 ﻿using Final_APITesting.Framework.Models;
 using FluentAssertions;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace Final_APITesting.Tests
 {
@@ -22,6 +23,27 @@ namespace Final_APITesting.Tests
             users.Should().NotBeNull();
             users.Count().Should().Be(10);
 
+        }
+        //
+        [Test]
+        public async Task CreateUser()
+        {
+            var newUser = new
+            {
+                name = $"{System.Guid.NewGuid()}",
+                email = $"{System.Guid.NewGuid()}@gmail.com",
+                gender = "male",
+                status = "active"
+            };
+
+            var jsonBody = new StringContent(JsonConvert.SerializeObject(newUser), Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("users", jsonBody);
+
+            response.StatusCode.ToString().Should().Be("Created");
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            var createdUser = JsonConvert.DeserializeObject<UserResponse>(responseData);
         }
     }
 }
